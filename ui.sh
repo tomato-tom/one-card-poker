875b4349d233a2aa6c5d983e8fd7e1f3d6e9f6ad
tomato-tom <hajimeee49@gmail.com>
2024-08-04 23:15:11 +0900

    first commit

---
+++ ui.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# poker UI プロトタイプ
+
+dealer_money=99
+dealer_bet=1
+pot=2
+player_money=99
+player_bet=1
+message="place your bet"
+
+echo
+echo " ♠One♣Card♡Poker♢"
+echo
+echo "    Dealer ($dealer_money)"
+echo "     [**]   $dealer_bet"
+echo
+echo "       $pot"
+echo
+echo "     [A♠]   $player_bet"
+echo "    Player ($player_money)"
+echo
+echo "  $message"
+
