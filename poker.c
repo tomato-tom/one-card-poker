767e501dc11f406fb7f4e3d813bf0492767893ff
tomato-tom <hajimeee49@gmail.com>
2024-08-11 23:22:07 +0900

    modified poker.c

--- poker.c
+++ poker.c
@@ -7,18 +7,22 @@
 #define DECK_SIZE 13
 #define INITIAL_MONEY 100
 #define ANTE 1
-#define MIN_BET_CARD 8
+#define MIN_BET_CARD 9
+#define MAX_BLUFF_CARD 3
 #define MIN_CALL_CARD 6
 #define CLEAR printf("\e[H\e[2J")
+#define CYAN "\033[36m"
+#define MAGENTA "\033[35m"
+#define RESET "\033[39m"
 
 const char *deck[DECK_SIZE] = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
-const char *suits[4] = {"♠", "♣", "♢", "♡"};
+const char *suits[4] = {"♠", "♣", "♦", "♥"};
 
 typedef enum {
-    FOLD = -1
+    FOLD = -1,
     PASS = 0,  // check
     CALL = 1,
-    BET  = 2,
+    BET  = 2
 } Action;
 
 typedef enum {
@@ -36,28 +40,40 @@ typedef struct {
     int player_money;
     int computer_money;
     int pot;
+    int winner;
 } GameState;
 
-void display_ui(GameState *state, const char *message, int winner);
+void display_ui(GameState *state, const char *message);
 void initialize_game(GameState *state);
-Action get_player_action(GameState *state, Action computer_action, int winner);
-Action get_computer_action(GameState *state, Action player_action, int winner);
+void deal_card(GameState *state);  //new
+void put_ante(GameState *state);   //new
+void play_round(GameState *state);
+Action get_player_action(GameState *state, Action computer_action);
+Action get_computer_action(GameState *state, Action player_action);
 void update_pot(GameState *state, Player player, Action action);
 Player showdown(GameState *state);
-void play_round(GameState *state);
+void determine_winner(GameState *state);
 
 
 int main() {
-    GameState state;
     srand(time(NULL));
 
-    char *message = "ゲームを始めます\n";
-    int winner = 0;
-    initialize_game(&state);
-    display_ui(&state, message, winner);
+    GameState state;
+    state.player_money = INITIAL_MONEY;
+    state.computer_money = INITIAL_MONEY;
 
     while (state.player_money > 0 && state.computer_money > 0) {
+        initialize_game(&state);
+
+        char *message = "ゲームを始めます\n";
+        display_ui(&state, message);
+        sleep(1);
+
+        deal_card(&state);
+        put_ante(&state);
         play_round(&state);
+        determine_winner(&state);
+
         printf("次のラウンドを始めますか？ (y/n): ");
         char choice;
         scanf(" %c", &choice);
@@ -69,169 +85,183 @@ int main() {
 }
 
 void initialize_game(GameState *state) {
-    state->player_money = INITIAL_MONEY;
-    state->computer_money = INITIAL_MONEY;
-    state->computer_card = -1;
-    state->player_card = -1;
+        state->winner = 0;
+        state->pot = 0;
+        state->player_bet = 0;
+        state->computer_bet = 0;
+        state->player_card = -1;
+        state->computer_card = -1;
 }
 
-void display_ui(GameState *state, const char *message, int winner) {
-    char playerCard[5];
-    char computerCard[5];
+void display_ui(GameState *state, const char *message) {
+    char playerCard[7];
+    char computerCard[7];
     if (state->player_card == -1) {
-        sprintf(playerCard, "  ");
+        sprintf(playerCard, "   ");
     } else {
-        sprintf(playerCard, "%s%s", deck[state->player_card], suits[state->suit]);
+        sprintf(playerCard, "%s %s", deck[state->player_card], suits[state->suit]);
     }
     if (state->computer_card == -1) {
-        sprintf(computerCard, "  ");
-    } else if (!winner) {
-        sprintf(computerCard, "**");
+        sprintf(computerCard, "   ");
+    } else if (!state->winner) {
+        sprintf(computerCard, "* *");
     } else {
-        sprintf(computerCard, "%s%s", deck[state->computer_card], suits[state->suit]);
+        sprintf(computerCard, "%s %s", deck[state->computer_card], suits[state->suit]);
     }
 
-    system("clear");
     CLEAR;
+    printf("\a");
     printf("\n");
-    printf("  One Card Poker ♠ ♣ ♡ ♢\n");
+    printf("       One Card Poker ♠ ♣ ♡ ♢\n");
     printf("\n");
-    printf("    Dealer ($%d)\n", state->computer_money);
-    printf("     [%s]   $%d\n", computerCard, state->computer_bet);
-    printf("\n");
-    printf("\n");
-    printf("     pot ($%d)\n", state->pot);
+    printf("         Dealer ($%s%d%s)\n", MAGENTA, state->computer_money, RESET);
+    printf("          %s[%s]%s   $%d\n", CYAN, computerCard, RESET, state->computer_bet);
     printf("\n");
     printf("\n");
-    printf("     [%s]   $%d\n", playerCard, state->player_bet);
-    printf("    Player ($%d)\n", state->player_money);
+    printf("          pot ($%s%d%s)\n", MAGENTA, state->pot, RESET);
+    printf("\n");
+    printf("\n");
+    printf("          %s[%s]%s   $%d\n", CYAN, playerCard, RESET, state->player_bet);
+    printf("         Player ($%s%d%s)\n", MAGENTA, state->player_money, RESET);
+    printf("\n");
     printf("\n");
     printf("%s\n", message);
 }
 
-void play_round(GameState *state) {
+void deal_card(GameState *state) {
+    char message[100];
+
+    state->suit = rand() % 4;
+
+    // playerのカードを１枚配る
     state->player_card = rand() % DECK_SIZE;
+    sprintf(message, "あなたのカード: %s%s", deck[state->player_card], suits[state->suit]);
+    display_ui(state, message);
+    sleep(1);
+
+    // 残りの１２枚からcomputerのカードを１枚配る
     int card = rand() % (DECK_SIZE - 1);
     if (card >= state->player_card) card++;
     state->computer_card = card;
-    state->suit = rand() % 4;
+    display_ui(state, message);
+    sleep(1);
+}
 
-    state->pot = ANTE * 2;
-    state->player_bet = ANTE;
-    state->computer_bet = ANTE;
-    state->player_money -= ANTE;
-    state->computer_money -= ANTE;
-
-    Player winner = 0;
+void put_ante(GameState *state) {
     char message[100];
-    sprintf(message, "");
-    display_ui(state, message, winner);
 
-    Action player_action = get_player_action(state, PASS, winner);
+    state->player_bet += ANTE;
+    state->player_money -= ANTE;
+    state->pot += ANTE;
+    sprintf(message, "あなたはアンティ$1置きました");
+    display_ui(state, message);
+    sleep(1);
+
+    state->computer_bet += ANTE;
+    state->computer_money -= ANTE;
+    state->pot += ANTE;
+    sprintf(message, "ディーラーがアンティ$1置きました");
+    display_ui(state, message);
+    sleep(1);
+}
+
+void play_round(GameState *state) {
+    char message[100];
+
+    Action player_action = get_player_action(state, 0);
+    sleep(1);
 
     if (player_action == FOLD) {
-        winner = COMPUTER;
+        state->winner = COMPUTER;
     } else {
-        Action computer_action = get_computer_action(state, player_action, winner);
+        Action computer_action = get_computer_action(state, player_action);
         sleep(1);
         if (computer_action == FOLD) {
-            winner = PLAYER;
+            state->winner = PLAYER;
         } else if (computer_action == BET && player_action != FOLD) {
-            player_action = get_player_action(state, BET, winner);
+            player_action = get_player_action(state, BET);
+            sleep(1);
             if (player_action == FOLD) {
-                winner = COMPUTER;
+                state->winner = COMPUTER;
             }
         }
     }
 
-    if (winner == 0) {
-        winner = showdown(state);
-    }
-
-    if (winner == PLAYER) {
-        sprintf(message, "あなたの勝ちです！ 獲得: $%d\n", state->pot);
-        state->player_money += state->pot;
-    } else if (winner == COMPUTER) {
-        sprintf(message, "コンピューターの勝ちです。獲得: $%d\n", state->pot);
-        state->computer_money += state->pot;
-    } else {
-        sprintf(message, "引き分けです。\n");
-        state->player_money += state->pot / 2;
-        state->computer_money += state->pot / 2;
-    }
-    display_ui(state, message, winner);
 }
 
-Action get_player_action(GameState *state, Action computer_action, int winner) {
+Action get_player_action(GameState *state, Action computer_action) {
     char action;
     char *message;
+
     if (computer_action == BET) {
         if (!state->player_money) return FOLD;
-        printf("アクションを選択してください (c: コール, f: フォールド): ");
+        message = "アクションを選択してください (c: コール, f: フォールド): ";
     } else {
         if (!state->player_money) return PASS;
-        printf("アクションを選択してください (p: パス, b: ベット): ");
+        message = "アクションを選択してください (p: パス, b: ベット): ";
     }
 
+    display_ui(state, message);
     scanf(" %c", &action);
 
     switch (action) {
         case 'c': 
-            message = "あなたはコールしました\n";
+            message = "あなたはコールしました";
             update_pot(state, PLAYER, CALL);
-            display_ui(state, message, winner);
+            display_ui(state, message);
             return CALL;
         case 'f': 
-            message = "あなたはフォールドしました\n";
+            message = "あなたはフォールドしました";
             update_pot(state, PLAYER, FOLD);
-            display_ui(state, message, winner);
+            display_ui(state, message);
             return FOLD;
         case 'b': 
-            message = "あなたはベットしました\n";
+            message = "あなたはベットしました";
             update_pot(state, PLAYER, BET);
-            display_ui(state, message, winner);
+            display_ui(state, message);
             return BET;
         case 'p': 
-            message = "あなたはパスしました\n";
+            message = "あなたはパスしました";
             update_pot(state, PLAYER, PASS);
-            display_ui(state, message, winner);
+            display_ui(state, message);
             return PASS;
         default:
-            message = "無効な入力です。パスとして扱います。\n";
+            message = "無効な入力はパスとして扱います";
             update_pot(state, PLAYER, PASS);
-            display_ui(state, message, winner);
+            display_ui(state, message);
             return PASS;
     }
 }
 
-Action get_computer_action(GameState *state, Action player_action, int winner) {
-    // 簡単な戦略: 8以上のカードを持っている場合、またはプレイヤーがパスした場合にベット
+Action get_computer_action(GameState *state, Action player_action) {
     char *message;
+
     if (player_action == BET) {
         if (!state->computer_money) return FOLD;
         if (state->computer_card >= MIN_CALL_CARD) {
         message = "コンピューターはコールしました\n";
         update_pot(state, COMPUTER, CALL);
-        display_ui(state, message, winner);
+        display_ui(state, message);
         return CALL;
         } else {
             message = "コンピューターはフォールドしました\n";
             update_pot(state, COMPUTER, FOLD);
-            display_ui(state, message, winner);
+            display_ui(state, message);
             return FOLD;
         }
     }
+
     if (player_action == PASS) {
         if (!state->computer_money) return PASS;
-        if (state->computer_card >= MIN_BET_CARD) {
+        if (state->computer_card >= MIN_BET_CARD || state->computer_card <= MAX_BLUFF_CARD) {
         message = "コンピューターはベットしました\n";
         update_pot(state, COMPUTER, BET);
-        display_ui(state, message, winner);
+        display_ui(state, message);
         return BET;
         } else {
         message = "コンピューターはパスしました\n";
         update_pot(state, COMPUTER, PASS);
-        display_ui(state, message, winner);
+        display_ui(state, message);
         return PASS;
         }
     }
@@ -251,11 +281,40 @@ void update_pot(GameState *state, Player player, Action action) {
 }
 
 Player showdown(GameState *state) {
-    printf("コンピューターのカード: %s\n", deck[state->computer_card]);
 
     if (state->player_card > state->computer_card) {
         return PLAYER;
     } else if (state->player_card < state->computer_card) {
         return COMPUTER;
+    }
+}
+
+void determine_winner(GameState *state) {
+    char message[100];
+
+    if (state->winner == 0) {
+        printf("コンピューターのカード: %s\n", deck[state->computer_card]);
+        usleep(300000);
+        printf("プレイヤーのカード: %s\n", deck[state->player_card]);
+        usleep(300000);
+        state->winner = showdown(state);
+    }
+    
+    state->player_bet = 0;
+    state->computer_bet = 0;
+    int pot = state->pot;
+
+    for (int chip = 1; chip <= pot; chip++) {
+        if (state->winner == PLAYER) {
+            sprintf(message, "あなたの勝ちです！ 獲得: $%d\n", chip);
+            state->player_money++;
+        } else if (state->winner == COMPUTER) {
+            sprintf(message, "コンピューターの勝ちです。獲得: $%d\n", chip);
+            state->computer_money++;
+        }
+
+        state->pot--;
+        display_ui(state, message);
+        usleep(300000);
     }
 }
